a1c29ddc9b8582067d24088599ce5eafdda15355
RenaldasZ <renald.zvega@gmail.com>
2023-06-01 20:19:17 +0300

    adden style to paginator

--- autoservice/service/views.py
+++ autoservice/service/views.py
@@ -1,5 +1,3 @@
-
-
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from . models import Car, OrderEntry, Service, Order
@@ -31,10 +29,6 @@ def car_list(request):
         'cars': paged_cars
     }
     return render(request, 'service/car_list.html', context=context)
-
-# def car_list(request):
-#     cars = Car.objects.all()
-#     return render(request, 'service/car_list.html', {'cars': cars})
 
 def car_detail(request, pk: int):
     car = get_object_or_404(Car, pk=pk)
