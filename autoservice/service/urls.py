c2fb9e70d31052b345776bc09f3c5a34e2e4c9d5
RenaldasZ <renald.zvega@gmail.com>
2023-06-01 10:37:42 +0300

    fixed order views and fixed extras

--- autoservice/service/urls.py
+++ autoservice/service/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import car_list
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cars/', car_list, name='car_list'),  
+    path('cars/', views.car_list, name='car_list'),  
     path('cars/<int:pk>/', views.car_detail, name='car_detail'),
+    
     path('orders/', views.OrderListView.as_view(), name='order_list'),
+    path('orders/<int:pk>/', views.order_detail, name='order_detail'),
 ]
--- autoservice/service/views.py
+++ autoservice/service/views.py
@@ -1,7 +1,8 @@
+
+
 from django.shortcuts import render, get_object_or_404
 from . models import Car, OrderEntry, Service, Order
 from django.views.generic import ListView
-from django.db.models import Sum
 
 def index(request):
     cars = Car.objects.all().count()
@@ -23,10 +24,15 @@ def car_detail(request, pk: int):
     car = get_object_or_404(Car, pk=pk)
     return render(request, 'service/car_detail.html', {'car': car})
 
+def order_detail(request, pk: int):
+    order = get_object_or_404(Order, pk=pk)
+    total_price = sum(entry.price for entry in order.order_entries.all())
+
+    return render(request, 'service/order_detail.html', {'order': order, 'total_price': total_price})
+
+
 class OrderListView(ListView):
     model = Order
     context_object_name = 'orders'
     template_name = 'service/order_list.html'
 
-    def get_queryset(self):
-        return super().get_queryset().prefetch_related('order_entries').annotate(total_price=Sum('order_entries__price'))
